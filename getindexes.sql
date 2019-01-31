8c235ac3f1eecf2aedba60653c4702dab03d93f8
mark.rusz@provident.hu <mark.rusz@provident.hu>
2019-01-31 11:41:20 +0100

    index building for temp tables added

--- getindexes.sql
+++ getindexes.sql
@@ -1,9 +1,10 @@
 declare @sql_to_get_clust_indexes varchar(MAX) = '',
 		@sql_to_turn_off_clust_indexes varchar(MAX) = '',
 		@db nvarchar(100) = 'CNFS_HUN', 
-		@full_table_name nvarchar(300) = 'CNFS_HUN.dbo.Agreement_Table',
+		@full_table_name nvarchar(300) = 'CNFS_HUN.dbo.Agreement_historic_data_Table',
 		@query_to_run nvarchar(max) = ''
 		declare @non_clustered_indexes table ([index_name] nvarchar(128))
+
 
 		
 select @sql_to_get_clust_indexes = 
@@ -22,13 +23,23 @@ insert into @non_clustered_indexes exec (@sql_to_get_clust_indexes)
 
 select * from @non_clustered_indexes;
 
-
+use Anonymizer;
 dbcc freeproccache
 dbcc dropcleanbuffers
-exec dbo.sp_SimpleAnonymizer @db='cnfs_hun', @schema='dbo', @tablep='agreement_table', @column_name = 'total_amount_payable'
+exec dbo.sp_SimpleAnonymizer @db='cnfs_hun', @schema='dbo', @tablep='agreement_table', @column_name = 'creation_week'
 
 use cnfs_hun;
 
 select rows, rowmodctr
 from cnfs_hun.sys.sysindexes with (nolock)
 where id = object_id('agreement_historic_data_table')
+
+use cnfs_hun;
+
+declare @PageSize int, @PageNumber int
+select @PageSize = 1000, @PageNumber = 10000
+SELECT *
+  FROM dbo.agreement_table a
+  order by a.Agreement_Id, a.Agreement_Db_Id
+  OFFSET @PageSize * (@PageNumber - 1) ROWS
+  FETCH NEXT @PageSize ROWS ONLY;
